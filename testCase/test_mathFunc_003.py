cdc579c2bb16653d0557fa65f800b1810fafaff9
xuexiaohua <18706726446@163.com>
2018-09-24 16:13:11 +0800

    python unittest

---
+++ testCase/test_mathFunc_003.py
@@ -0,0 +1,49 @@
+import unittest
+from sourceCode.mathFunc import *
+
+class TestMathFunc003(unittest.TestCase):
+    """Test mathfunc.py"""
+    @classmethod
+    def setUpClass(cls):
+        print("this setUpClass method only called once")
+
+    @classmethod
+    def tearDownClass(cls):
+        print("this tearDownClass method only called once")
+
+    def setUp(self):
+        print("setUp")
+
+    def tearDown(self):
+        print("tearDown")
+
+    def test_add(self):
+        """Test method add(a,b)"""
+        self.assertEqual(3,add(1,2))
+
+    def test_minus(self):
+        """Test Method minus(a,b)"""
+        self.assertEqual(1,minus(3,2))
+
+    def test_multi(self):
+        """Test Method multi(a,b)"""
+        self.assertEqual(2,multi(1,2))
+
+    def test_divide(self):
+        """Test Method divide(a,b)"""
+        self.assertEqual(2,divide(4,2))
+
+if __name__ == "__main__":
+    # 设置testCase有顺序，按设定顺序执行
+    # suit=unittest.TestSuite()
+    # tests=[TestMathFunc003("test_add"),TestMathFunc003("test_minus"),
+    #        TestMathFunc003("test_multi"),TestMathFunc003("test_divide")]
+    # suit.addTests(tests)
+    # unittest.TextTestRunner(verbosity=2).run(suit)
+
+    suit1=unittest.TestSuite()
+    suit1.addTest(TestMathFunc003("test_add"))
+    suit1.addTest(TestMathFunc003("test_minus"))
+    suit1.addTest(TestMathFunc003("test_multi"))
+    suit1.addTest(TestMathFunc003("test_divide"))
+    unittest.TextTestRunner(verbosity=2).run(suit1)
